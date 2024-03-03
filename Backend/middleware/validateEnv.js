a84d5e50b871b8493ea1116e0be1f20f0d454ce8
diveshkar <divesh.kar@gmail.com>
2024-03-03 22:52:22 +0530

    customize frontend

--- Backend/middleware/validateEnv.js
+++
@@ -1,7 +0,0 @@
-const { str, port, cleanEnv } = require("envalid");
-
-
-module.exports =  cleanEnv(process.env, {
-    DATABASE: str(),
-    PORT: port(),
-});
--- Backend/routes/messageRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {saveMessage} = require('../controller/messageController');
-
-const router = express.Router();
-
-router.post('/send-message', saveMessage);
-
-module.exports = router;
